0ec954b315be867aa70480275cd8b618334157ac
pdarko99 <padarko99@gmail.com>
2026-02-13 23:53:26 +0000

    added configuration annotation

--- src/main/java/com/ecommerce/ecommerce/config/CorsConfig.java
+++ src/main/java/com/ecommerce/ecommerce/config/CorsConfig.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
 public class CorsConfig {
 
     @Bean
